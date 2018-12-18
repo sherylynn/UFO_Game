c8a905d9357588c8f5f8110c550fc523cb11c052
sherylynn <352281674@qq.com>
2018-12-19 00:04:51 +0800

    chang update to LateUpdate

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,7 +11,7 @@ public class CameraController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
         transform.position = player.transform.position + offset;
 	}
 }
