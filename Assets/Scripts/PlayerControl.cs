e8d9c8e2cefae60ba20b93bd77bc646613be0bf9
sherylynn <352281674@qq.com>
2018-12-18 19:53:56 +0800

    add player control

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rg2d;
+	// Use this for initialization
+	void Start () {
+        rg2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical= Input.GetAxis("Vertical");
+        Vector2 moveForce = new Vector2(moveHorizontal, moveVertical);
+        rg2d.AddForce(moveForce*speed);
+    }
+}
