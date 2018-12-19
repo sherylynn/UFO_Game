2a809f128893c54f75f41b0d806fdabe8c058efc
sherylynn <352281674@qq.com>
2018-12-19 17:59:14 +0800

    add score text and win text

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,19 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour {
     public float speed;
     private Rigidbody2D rg2d;
+    public Text CountText;
+    public Text WinText;
+    private int _counts;
 	// Use this for initialization
 	void Start () {
         rg2d = GetComponent<Rigidbody2D>();
-	}
+        CountText.text = "Counts:";
+        _counts = 0;
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -26,6 +28,17 @@ public class PlayerControl : MonoBehaviour {
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            _counts = _counts + 1;
+            SetCountText();
+        }
+
+    }
+    void SetCountText()
+    {
+        CountText.text = "Counts:" + _counts.ToString();
+        if (_counts == 12)
+        {
+            WinText.gameObject.SetActive(true);
         }
     }
 }
